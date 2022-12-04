ea53133523bf949d0cc4df2a8f3f45456ca599d8
Julia Katharina Nee <juliakatharina77@icloud.com>
2022-12-04 22:52:43 +0100

    Stylinglog

--- src/compontens/Pages/Sign-up.js
+++ src/compontens/Pages/Sign-up.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import "../../App.css";
+import "./Sign-up.css";
 
 async function loginUser(credentials) {
   return fetch("http://localhost:8080/signup", {
@@ -24,92 +24,70 @@ export default function SignUp({ setToken }) {
     setToken(token);
   };
   return (
-    <main className="main">
-      <div className="container">
-        <section className="wrapper">
-          <div className="heading">
-            <h1 className="text-large"> Sign In</h1>
-            <p className="text-normal">
-              New User ?
-              <span>
-                <a href="#" className="text-links">
-                  Create a account
-                </a>
-              </span>
-            </p>
-          </div>
+    <div className="container">
+      <section className="wrapper">
+        <h1 className="text-large"> Sign In</h1>
 
-          <form
-            onSubmit={handleSubmit}
-            action=""
-            name="signin"
-            className="form"
-          >
-            <div className="input-controll">
-              <label for="text" className="input-label" hidden>
-                UserName
-              </label>
-              <input
-                className="input-flied"
-                type="text"
-                name="text"
-                required
-                id="text"
-                placeholder="UserName"
-                onChange={(e) => setUserName(e.target.value)}
-              />
-            </div>
-            <div className="input-controll">
-              <label for="password" className="input-label" hidden>
-                Password
-              </label>
+        <form onSubmit={handleSubmit} action="" name="signin" className="form">
+          <div className="input-controll">
+            <label className="input-label" hidden>
+              UserName
+            </label>
+            <input
+              className="input-field"
+              type="text"
+              name="text"
+              required
+              id="text"
+              placeholder="UserName"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="input-controll">
+            <label className="input-label">
               <input
                 type="password"
-                name="password"
-                id="password
-                "
                 className="input-field"
                 placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
               />
-            </div>
-            <div className="input-controll">
-              <a href="#" className="text-links">
-                Forgot Password
-              </a>
-              <button type="submit" className="input-submit">
-                Submit
-              </button>
-            </div>
-          </form>
-          <div className="striped">
-            <span className="method-line"></span>
-            <span className="striped-text">Or</span>
-            <span className="striped-line"></span>
+              <span class="eye" onclick="myFunction()">
+                <i id="hide1" class="far fa-eye"></i>
+                <i id="hide2" class="fas fa-eye-slash"></i>
+              </span>
+            </label>
           </div>
-          <div className="method">
-            <div className="method-controll">
-              <a href="#" className="method-action">
-                <i className="fa-brands fa-google"></i>
-                <span> Sign in with Google</span>
-              </a>
-            </div>
-            <div className="method-controll">
-              <a href="#" className="method-action">
-                <i className="fa-brands fa-facebook-f"></i>
-                <span> Sign in with Facebook</span>
-              </a>
-            </div>
-            <div className="method-controll">
-              <a href="#" className="method-action">
-                <i className="fa-brands fa-apple"></i>
-                <span> Sign in with Apple</span>
-              </a>
-            </div>
+          <button type="submit" className="input-submit">
+            Submit
+          </button>
+        </form>
+        <div className="striped">
+          <span className="method-line"></span>
+          <span className="striped-text">Or</span>
+          <span className="striped-line"></span>
+        </div>
+        <div className="method">
+          <div className="method-controll">
+            <a href="#" className="method-action">
+              <i className="fa-brands fa-google"></i>
+              <span className="text-log"> Sign in with Google</span>
+            </a>
           </div>
-        </section>
-      </div>
-    </main>
+          <div className="method-controll">
+            <a href="#" className="method-action">
+              <i className="fa-brands fa-facebook-f"></i>
+              <span className="text-log"> Sign in with Facebook</span>
+            </a>
+          </div>
+          <div className="method-controll">
+            <a href="#" className="method-action">
+              <i className="fa-brands fa-apple"></i>
+              <span className="text-log"> Sign in with Apple</span>
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
