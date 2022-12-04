0420f2efafaea9f2edb458a17f9aa02379fd16d9
Julia Katharina Nee <juliakatharina77@icloud.com>
2022-12-04 22:56:41 +0100

    Updatebackgroundlogin

--- src/compontens/Pages/Sign-up.js
+++ src/compontens/Pages/Sign-up.js
@@ -24,69 +24,76 @@ export default function SignUp({ setToken }) {
     setToken(token);
   };
   return (
-    <div className="container">
-      <section className="wrapper">
-        <h1 className="text-large"> Sign In</h1>
+    <div className="background">
+      <div className="container">
+        <section className="wrapper">
+          <h1 className="text-large"> Sign In</h1>
 
-        <form onSubmit={handleSubmit} action="" name="signin" className="form">
-          <div className="input-controll">
-            <label className="input-label" hidden>
-              UserName
-            </label>
-            <input
-              className="input-field"
-              type="text"
-              name="text"
-              required
-              id="text"
-              placeholder="UserName"
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
-          <div className="input-controll">
-            <label className="input-label">
+          <form
+            onSubmit={handleSubmit}
+            action=""
+            name="signin"
+            className="form"
+          >
+            <div className="input-controll">
+              <label className="input-label" hidden>
+                UserName
+              </label>
               <input
-                type="password"
                 className="input-field"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
+                type="text"
+                name="text"
+                required
+                id="text"
+                placeholder="UserName"
+                onChange={(e) => setUserName(e.target.value)}
               />
-              <span class="eye" onclick="myFunction()">
-                <i id="hide1" class="far fa-eye"></i>
-                <i id="hide2" class="fas fa-eye-slash"></i>
-              </span>
-            </label>
+            </div>
+            <div className="input-controll">
+              <label className="input-label">
+                <input
+                  type="password"
+                  className="input-field"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <span class="eye" onclick="myFunction()">
+                  <i id="hide1" class="far fa-eye"></i>
+                  <i id="hide2" class="fas fa-eye-slash"></i>
+                </span>
+              </label>
+            </div>
+            <button type="submit" className="input-submit">
+              Submit
+            </button>
+          </form>
+          <div className="striped">
+            <span className="method-line"></span>
+            <span className="striped-text">Or</span>
+            <span className="striped-line"></span>
           </div>
-          <button type="submit" className="input-submit">
-            Submit
-          </button>
-        </form>
-        <div className="striped">
-          <span className="method-line"></span>
-          <span className="striped-text">Or</span>
-          <span className="striped-line"></span>
-        </div>
-        <div className="method">
-          <div className="method-controll">
-            <a href="#" className="method-action">
-              <i className="fa-brands fa-google"></i>
-              <span className="text-log"> Sign in with Google</span>
-            </a>
+          <div className="method">
+            <div className="method-controll">
+              <a href="#" className="method-action">
+                <i className="fa-brands fa-google"></i>
+                <span className="text-log"> Sign in with Google</span>
+              </a>
+            </div>
+            <div className="method-controll">
+              <a href="#" className="method-action">
+                <i className="fa-brands fa-facebook-f"></i>
+                <span className="text-log"> Sign in with Facebook</span>
+              </a>
+            </div>
+            <div className="method-controll">
+              <a href="#" className="method-action">
+                <i className="fa-brands fa-apple"></i>
+                <span className="text-log"> Sign in with Apple</span>
+              </a>
+            </div>
           </div>
-          <div className="method-controll">
-            <a href="#" className="method-action">
-              <i className="fa-brands fa-facebook-f"></i>
-              <span className="text-log"> Sign in with Facebook</span>
-            </a>
-          </div>
-          <div className="method-controll">
-            <a href="#" className="method-action">
-              <i className="fa-brands fa-apple"></i>
-              <span className="text-log"> Sign in with Apple</span>
-            </a>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 }
